1b1732b56a64059253b83a3fe788ed7109fed3cd
Michael Gianoulakis <mgiano02@gmail.com>
2023-04-06 16:16:02 -0700

    Added attendee login interface and updated application security

---
+++ ghi/js/attend-conference.js
@@ -0,0 +1,50 @@
+window.addEventListener('DOMContentLoaded', async () => {
+    const selectTag = document.getElementById('conference');
+
+    const url = 'http://localhost:8000/api/conferences/';
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+
+      for (let conference of data.conferences) {
+        const option = document.createElement('option');
+        option.value = conference.href;
+        option.innerHTML = conference.name;
+        selectTag.appendChild(option);
+      }
+
+      // Here, add the 'd-none' class to the loading icon
+      const loading = document.getElementById("loading-conference-spinner")
+      loading.classList.add("d-none")
+      // Here, remove the 'd-none' class from the select tag
+      selectTag.classList.remove("d-none");
+    }
+
+
+    const formTag = document.getElementById('create-attendee-form');
+    formTag.addEventListener('submit', async event => {
+      event.preventDefault();
+      const formData = new FormData(formTag);
+      const json = JSON.stringify(Object.fromEntries(formData));
+        console.log(json);
+        const attendeeUrl = 'http://localhost:8001/api/attendees/';
+        const fetchConfig = {
+        method: "post",
+        body: json,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        };
+        const response = await fetch(attendeeUrl, fetchConfig);
+        if (response.ok) {
+        formTag.reset();
+        const newAttendee = await response.json();
+
+        const success = document.getElementById("success-message");
+        success.classList.remove("d-none");
+        formTag.classList.add("d-none");
+
+        }
+    });
+
+  });
--- monolith/conference_go/settings.py
+++ monolith/conference_go/settings.py
@@ -39,6 +39,7 @@ AUTH_USER_MODEL = "accounts.User"
 # Application definition
 
 INSTALLED_APPS = [
+    "djwto",
     "accounts.apps.AccountsConfig",
     "events.apps.EventsConfig",
     "presentations.apps.PresentationsConfig",
@@ -64,6 +65,11 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+DJWTO_MODE = "TWO-COOKIES"
+DJWTO_ACCESS_TOKEN_LIFETIME = None
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "conference_go.urls"
 
